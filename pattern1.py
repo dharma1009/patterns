b6a212095573f8bf3bb1e1fbfe606cf02f014215
dharma1009 <mdharma1009@gmail.com>
2026-09-10 07:39:43 +0530

    basic

--- pattern1.py
+++ pattern1.py
@@ -3,3 +3,15 @@ for i in range(1,n+1,1):
     for j in range(1,n+1,1):
         print("*",end=" ")
     print()
+print()
+n = 5
+for i in range(1,n+1,1):
+    for j in range(1,n+1,1):
+        print(i,end=" ")
+    print()
+print()
+n = 5
+for i in range(1,n+1,1):
+    for j in range(1,n+1,1):
+        print(j,end=" ")
+    print()
